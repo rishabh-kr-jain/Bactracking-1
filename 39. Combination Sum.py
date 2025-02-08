7c1794beb3cb30e36ac888a2977d4e5573085946
Rishabh <24757929+rishabh-kr-jain@users.noreply.github.com>
2025-02-08 09:07:03 -0800

    Create 39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,26 @@
+#Space: O(height of recursion stack)
+#Time: O(n)
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.final= list(list())
+        self.recurse(candidates, target,list(),0)
+        return self.final
+
+    def recurse(self, candidates, target, path, index):
+        if target < 0 or index == len(candidates):
+            return
+        
+        if target==0:
+            self.final.append(path.copy())
+            return 
+        #recurse
+        self.recurse(candidates, target, path, index+1)
+        #action
+        path.append(candidates[index])
+        self.recurse(candidates, target- candidates[index], path, index)
+        #backtrack
+        path.pop()
+        
+        return
+
+        
